93ca44632e983250a28e6fe002abf8de1a3574e9
Kingdon Barrett <kbarrett@metrixmatrix.com>
2014-06-13 09:37:20 -0400

    installing system rvm means also using it
    chef gems already installed for 1.9.3 will not work against new system
    default ruby 2.1.2 from rvm.  Now bootstrapped into rvm it's necessary
    to purge out the older ruby debs and transition to new cookbook/recipes.

--- install.sh
+++ install.sh
@@ -2,15 +2,21 @@
 
 # This runs as root on the server
 
-chef_binary=/var/lib/gems/1.9.1/gems/chef-11.12.8/bin/chef-solo
+chef_binary=/usr/local/rvm/gems/ruby-2.1.2/bin/chef-solo
+rvm_profile=/etc/profile.d/rvm.sh
+if [ -f $rvm_profile ]; then
+  . $rvm_profile
+fi
 
+export rvmsudo_secure_path=1
 # Are we on a vanilla system?
 if ! test -f "$chef_binary"; then
   export DEBIAN_FRONTEND=noninteractive
   aptitude update && apt-get -o Dpkg::Options::="--force-confnew" \
     --force-yes -fuy dist-upgrade &&
-  aptitude install -y ruby1.9.1 ruby1.9.1-dev make &&
-  sudo gem1.9.1 install --no-rdoc --no-ri chef --version 11.12.8
+  apt-get autoremove --purge -y ruby1.9.1 ruby1.9.1-dev &&
+  apt-get autoremove --purge -y &&
+  rvmsudo gem install --no-rdoc --no-ri chef --version 11.12.8
 fi &&
 
 "$chef_binary" -c solo.rb -j solo.json
